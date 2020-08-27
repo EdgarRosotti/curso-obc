c0575930096e84a7c05b6b935a4be0d71de38508
Edgar <edgarrosotti@hotmail.com>
2020-08-27 20:53:01 -0300

    Edgar

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Controller {
+
+    @GetMapping("/cliente")
+    public String getClientes(){
+        return "Vai algum dia retornar do BD todos os clientes!";
+    }
+
+
+    @GetMapping("/cliente/{codigo}")
+    public String getCliente(@PathVariable int codigo) {
+        
+
+        if(codigo > 0)
+            return "Vai algum dia retornar um cliente " + codigo;
+        else
+            return "Código inválido" + codigo;
+    }
+}
